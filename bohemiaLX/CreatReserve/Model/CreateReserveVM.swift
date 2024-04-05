926a8b0379eaa3f2385f5fa1f991db7a56d22190
pedroriibeiro <pedroriibeiro.dev@gmail.com>
2024-04-05 15:31:51 +0100

    Criei a viewModel e comecei a trabalhar a enviar os dados para o firebaseStore

---
+++ bohemiaLX/CreatReserve/Model/CreateReserveVM.swift
@@ -0,0 +1,12 @@
+//
+//  CreateReserveVM.swift
+//  bohemiaLX
+//
+//  Created by Pedro Ribeiro on 05/04/2024.
+//
+
+import UIKit
+
+class CreateReserveVM: NSObject {
+
+}
---
+++ bohemiaLX/CreatReserve/Model/CreateReserveViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CreateReserveViewModel.swift
+//  bohemiaLX
+//
+//  Created by Pedro Ribeiro on 05/04/2024.
+//
+
+import UIKit
+
+class CreateReserveViewModel: NSObject {
+
+}
